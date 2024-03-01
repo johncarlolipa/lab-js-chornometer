a998efd26a2aa9d7cc8e92f4f4030de564743e60
John Carlo Lipa <johncarlolipa@gmail.com>
2024-03-01 20:28:28 +0800

    iteration 1 done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,59 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    (this.currentTime = 0), (this.intervalID = null);
   }
 
   start(callback) {
     // ... your code goes here
+    if (callback) {
+      this.intervalID = setInterval(() => {
+        this.currentTime++;
+        callback();
+      }, 1000);
+    } else {
+      this.intervalID = setInterval(() => {
+        this.currentTime++;
+      }, 1000);
+    }
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    return value < 10 ? '0' + value : value.toString();
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalID);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
+    // reset HTML
+
+    const minutesEl = document.getElementById('minutes');
+    const secondsEl = document.getElementById('seconds');
+    minutesEl.innerHTML = '00';
+    secondsEl.innerHTML = '00';
   }
 
   split() {
     // ... your code goes here
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
   }
 }
 
